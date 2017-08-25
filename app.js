f7ba93bde373d1a6adb76e124f2521825394a556
Ara <elishaara@gmail.com>
2017-08-25 11:36:35 +0900

    add app.js and index.js

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8000;
+
+var router = require('./router')(app);
+
+var server = app.listen(port, function() {
+	console.log('Express server has started on port ' + port);
+});
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function() {
+	console.log('Connected to mongod server');
+});
+
+mongoose.connect('mongodb://localhost/mongodb_tutorial');
